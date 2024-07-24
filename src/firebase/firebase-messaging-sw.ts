cf6a6bb854f5cbf9bcbc0226793c1b0700cb3d1b
kyw0716 <kyw0716@naver.com>
2024-07-24 23:39:02 +0900

    chore: I hate ios

--- src/firebase/firebase-messaging-sw.ts
+++ src/firebase/firebase-messaging-sw.ts
@@ -14,10 +14,29 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const messaging = getMessaging(app);
 
+function checkMobile() {
+  var varUA = navigator.userAgent.toLowerCase(); //userAgent 값 얻기
+
+  if (varUA.indexOf('android') > -1) {
+    //안드로이드
+    return 'android';
+  } else if (
+    varUA.indexOf('iphone') > -1 ||
+    varUA.indexOf('ipad') > -1 ||
+    varUA.indexOf('ipod') > -1
+  ) {
+    //IOS
+    return 'ios';
+  } else {
+    //아이폰, 안드로이드 외
+    return 'other';
+  }
+}
+
 async function requestPermission() {
   alert('권한 요청 중...');
 
-  if ('Notification' in window) {
+  if (checkMobile() !== 'ios') {
     const permission = await Notification.requestPermission();
     if (permission === 'denied') {
       alert('알림 권한 허용 안됨');
