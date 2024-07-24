a624d219e66fe4b8d92c6b859f3e5192975f1b35
kyw0716 <kyw0716@naver.com>
2024-07-24 23:36:08 +0900

    chore: 모바일에서 어떤 에러가 발생하는지 확인하기

--- src/firebase/firebase-messaging-sw.ts
+++ src/firebase/firebase-messaging-sw.ts
@@ -16,7 +16,8 @@ const messaging = getMessaging(app);
 
 async function requestPermission() {
   alert('권한 요청 중...');
-  try {
+
+  if ('Notification' in window) {
     const permission = await Notification.requestPermission();
     if (permission === 'denied') {
       alert('알림 권한 허용 안됨');
@@ -41,26 +42,26 @@ async function requestPermission() {
       console.log('메시지가 도착했습니다.', payload);
       // ...
     });
-  } catch (error) {
-    try {
-      const token = await getToken(messaging, {
-        vapidKey: process.env.REACT_APP_VAPID_KEY,
-      });
+  }
+  try {
+    const token = await getToken(messaging, {
+      vapidKey: process.env.REACT_APP_VAPID_KEY,
+    });
 
-      if (token) {
-        window.navigator.clipboard.writeText(token);
+    if (token) {
+      window.navigator.clipboard.writeText(token);
 
-        alert(token);
-      } else {
-        alert('Can not get Token');
-      }
-
-      onMessage(messaging, (payload) => {
-        alert(payload.notification?.body);
-      });
-    } catch (error) {
-      alert(error);
+      alert(token);
+    } else {
+      alert('Can not get Token');
     }
+
+    onMessage(messaging, (payload) => {
+      console.log('메시지가 도착했습니다.', payload);
+      // ...
+    });
+  } catch (error) {
+    alert(error);
   }
 }
 
